17dbedc38910bbef9f043b3e489a9ec2a3138ea4
Yeseo Kim <chinkl850@gmail.com>
2025-12-20 15:20:55 +0900

    feat: Redis 및 Redisson 환경 설정

---
+++ src/main/kotlin/kr/hhplus/be/server/config/RedissonConfig.kt
@@ -0,0 +1,28 @@
+package kr.hhplus.be.server.config
+
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RedissonConfig(
+    @param:Value("\${spring.data.redis.host}")
+    private val host: String,
+    @param:Value("\${spring.data.redis.port}")
+    private val port: Int
+) {
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer().apply {
+            address = "redis://$host:$port"
+        }
+
+        return Redisson.create(config)
+    }
+
+}
